9245b7c41fe3f8e2f97e4907646609c6745d4c10
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-04-17 12:01:07 +0500

    Update var.js

--- variables/var.js
+++ variables/var.js
@@ -79,4 +79,27 @@
 
 
 
-// all the three variables (let,var,const) are functional scoped it means if a variable si declared inside a function, it is not accessable outside of the function;
+// all the three variables (let,var,const) are functional scoped it means if a variable is declared inside a function,
+// it is not accessable outside of the function;
+
+
+
+// using (let or var) if you reassign value inside the function. you can also access this change outside of the funciton
+// but if you redeclare a value inside the function. now this is only funcitonal scoped;
+
+
+var a = 45;
+console.log(a);    // 45
+function hello () {
+    a = 'inside';
+    console.log(a);    // inside
+}
+hello();
+console.log(a);   // inside
+
+
+
+
+//but if you only put var before variable 'a' indide the function 'hello' you can see the change.
+//now a = 'inside' is functional scoped and outside of the funciton, their is still a = 45;
+// remember this method applies on both (var and let);
